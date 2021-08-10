c8cd5f5bf9f9428c31f869776cac45c04e323e2d
Victor Lee <fornamoo@gmail.com>
2021-08-10 22:44:15 +0900

    update

---
+++ dist/lib/markdown/rules.d.ts
@@ -0,0 +1,6 @@
+import markdownit from "markdown-it";
+export default function rules({ embeds, rules }: {
+    embeds: any;
+    rules?: {} | undefined;
+}): markdownit;
+//# sourceMappingURL=rules.d.ts.map
