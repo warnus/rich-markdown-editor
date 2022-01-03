422b69b679a57f10d8ce3b0819251175889ac6f4
Wooseung Lee <fornamoo@gmail.com>
2022-01-03 04:16:37 +0000

    update dist

---
+++ dist/commands/insertAllFiles.js
@@ -0,0 +1,48 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+const types_1 = require("../types");
+const insertAllFiles = function (view, event, pos, files, options) {
+    if (files.length === 0)
+        return;
+    const { dictionary, uploadFile, onFileUploadStart, onFileUploadStop, onShowToast, } = options;
+    if (!uploadFile) {
+        console.warn("uploadFile callback must be defined to handle file uploads.");
+        return;
+    }
+    event.preventDefault();
+    if (onFileUploadStart)
+        onFileUploadStart();
+    let complete = 0;
+    const { state } = view;
+    const { from, to } = state.selection;
+    console.log("state.selection: " + state.selection + "," + from + "," + to);
+    for (const file of files) {
+        var placeholder = `[${file.name} uploading...]`;
+        view.dispatch(view.state.tr
+            .insertText(placeholder, from, to - 1));
+        uploadFile(file)
+            .then(src => {
+            const title = file.name;
+            const href = src;
+            view.dispatch(view.state.tr
+                .delete(from, to + placeholder.length)
+                .insertText(title, from, to)
+                .addMark(from, to + title.length, state.schema.marks.link.create({ href })));
+        })
+            .catch(error => {
+            console.error(error);
+            if (onShowToast) {
+                onShowToast(dictionary.fileUploadError, types_1.ToastType.Error);
+            }
+        })
+            .finally(() => {
+            complete++;
+            if (complete === files.length) {
+                if (onFileUploadStop)
+                    onFileUploadStop();
+            }
+        });
+    }
+};
+exports.default = insertAllFiles;
+//# sourceMappingURL=insertAllFiles.js.map
