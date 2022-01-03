7b1355370a30581dda10b62da662846d864acccd
Wooseung Lee <fornamoo@gmail.com>
2022-01-03 14:35:36 +0000

    test

--- dist/nodes/File.js
+++ dist/nodes/File.js
@@ -1,10 +1,31 @@
 "use strict";
+var __createBinding = (this && this.__createBinding) || (Object.create ? (function(o, m, k, k2) {
+    if (k2 === undefined) k2 = k;
+    Object.defineProperty(o, k2, { enumerable: true, get: function() { return m[k]; } });
+}) : (function(o, m, k, k2) {
+    if (k2 === undefined) k2 = k;
+    o[k2] = m[k];
+}));
+var __setModuleDefault = (this && this.__setModuleDefault) || (Object.create ? (function(o, v) {
+    Object.defineProperty(o, "default", { enumerable: true, value: v });
+}) : function(o, v) {
+    o["default"] = v;
+});
+var __importStar = (this && this.__importStar) || function (mod) {
+    if (mod && mod.__esModule) return mod;
+    var result = {};
+    if (mod != null) for (var k in mod) if (k !== "default" && Object.prototype.hasOwnProperty.call(mod, k)) __createBinding(result, mod, k);
+    __setModuleDefault(result, mod);
+    return result;
+};
 var __importDefault = (this && this.__importDefault) || function (mod) {
     return (mod && mod.__esModule) ? mod : { "default": mod };
 };
 Object.defineProperty(exports, "__esModule", { value: true });
 const prosemirror_inputrules_1 = require("prosemirror-inputrules");
 const toggleWrap_1 = __importDefault(require("../commands/toggleWrap"));
+const outline_icons_1 = require("outline-icons");
+const React = __importStar(require("react"));
 const react_dom_1 = __importDefault(require("react-dom"));
 const Node_1 = __importDefault(require("./Node"));
 const files_1 = __importDefault(require("../rules/files"));
@@ -75,16 +96,13 @@ class File extends Node_1.default {
                 });
                 let component;
                 if (node.attrs.style === "tip") {
-                    component = color;
-                    "currentColor" /  > ;
+                    component = React.createElement(outline_icons_1.StarredIcon, { color: "currentColor" });
                 }
                 else if (node.attrs.style === "warning") {
-                    component = color;
-                    "currentColor" /  > ;
+                    component = React.createElement(outline_icons_1.WarningIcon, { color: "currentColor" });
                 }
                 else {
-                    component = color;
-                    "currentColor" /  > ;
+                    component = React.createElement(outline_icons_1.InfoIcon, { color: "currentColor" });
                 }
                 const icon = document.createElement("div");
                 icon.className = "icon";
