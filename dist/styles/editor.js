187ada23231c72455deb31965f686c7cdaa598ca
Wooseung Lee <fornamoo@gmail.com>
2022-01-07 11:10:04 +0900

    change loader

--- dist/styles/editor.js
+++ dist/styles/editor.js
@@ -82,17 +82,75 @@ exports.StyledEditor = styled_components_1.default("div") `
     clear: initial;
   }
 
-  .loader {
-    border: 5px solid #f3f3f3;
-    border-radius: 50%;
-    border-top: 5px solid #3498db;
-    width: 2.5em;
-    height: 2.5em;
-    margin: 80px auto;
-    position: relative;
-    -webkit-animation: spin 2s linear infinite; /* Safari */
-    animation: spin 2s linear infinite;
+  // .loader {
+  //   border: 5px solid #f3f3f3;
+  //   border-radius: 50%;
+  //   border-top: 5px solid #3498db;
+  //   width: 2.5em;
+  //   height: 2.5em;
+  //   margin: 80px auto;
+  //   position: relative;
+  //   -webkit-animation: spin 2s linear infinite; /* Safari */
+  //   animation: spin 2s linear infinite;
+  // }
+
+  .loader,
+.loader:before,
+.loader:after {
+  border-radius: 50%;
+  width: 2.5em;
+  height: 2.5em;
+  -webkit-animation-fill-mode: both;
+  animation-fill-mode: both;
+  -webkit-animation: load7 1.8s infinite ease-in-out;
+  animation: load7 1.8s infinite ease-in-out;
+}
+.loader {
+  color: #00008B;
+  font-size: 10px;
+  margin: 80px auto;
+  position: relative;
+  text-indent: -9999em;
+  -webkit-transform: translateZ(0);
+  -ms-transform: translateZ(0);
+  transform: translateZ(0);
+  -webkit-animation-delay: -0.16s;
+  animation-delay: -0.16s;
+}
+.loader:before,
+.loader:after {
+  content: '';
+  position: absolute;
+  top: 0;
+}
+.loader:before {
+  left: -3.5em;
+  -webkit-animation-delay: -0.32s;
+  animation-delay: -0.32s;
+}
+.loader:after {
+  left: 3.5em;
+}
+@-webkit-keyframes load7 {
+  0%,
+  80%,
+  100% {
+    box-shadow: 0 2.5em 0 -1.3em;
   }
+  40% {
+    box-shadow: 0 2.5em 0 0;
+  }
+}
+@keyframes load7 {
+  0%,
+  80%,
+  100% {
+    box-shadow: 0 2.5em 0 -1.3em;
+  }
+  40% {
+    box-shadow: 0 2.5em 0 0;
+  }
+}
   
   @keyframes spin {
     0% { transform: rotate(0deg); }
