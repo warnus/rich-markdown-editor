2de76b77d8b9da985b3aa2dba8ed7eb2ff12e788
Wooseung Lee <fornamoo@gmail.com>
2022-01-10 03:20:37 +0000

    test

--- src/nodes/FileDoc.tsx
+++ src/nodes/FileDoc.tsx
@@ -192,7 +192,7 @@ export default class File extends Node {
   toMarkdown(state, node) {
     state.write("\n@@@");
     state.renderContent(node);
-    state.ensureNewLine();
+    // state.ensureNewLine();
     // state.write("[" +  
       // state.esc(node.attrs.alt) + "]" + "(" +
       // state.esc(node.attrs.src) + ")"
